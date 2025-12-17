7923d8781a54fda1f65e2a63d8b1a60a69d448e8
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-12-17 15:26:06 +0200

    fix: add response schema, router prefix

--- app/routes/task.py
+++ app/routes/task.py
@@ -1,19 +1,23 @@
 from fastapi import APIRouter, HTTPException
 from app.services.task_manager import TaskManager
-from app.schemas.task import TaskModel
-router = APIRouter()
+from app.schemas.task import TaskModel, TaskRead
+
+router = APIRouter(
+  prefix='/tasks',
+  tags=["tasks"]
+  )
 tm = TaskManager()
 
-@router.get('/tasks')
+@router.get('/', response_model=list[TaskRead])
 async def get_tasks():
   return tm.to_dict()
 
-@router.post('/tasks')
+@router.post('/', response_model=TaskRead)
 async def post_task(task: TaskModel):
   added_task = tm.add_task(task.title)
   return tm._task_to_dict(added_task)
 
-@router.get('/tasks/{id}')
+@router.get('/{id}', response_model=TaskRead)
 async def get_task_by_id(id: int):
   try:
     task = tm.get_task(id)
@@ -24,21 +28,23 @@ async def get_task_by_id(id: int):
       detail = f"Task with id={id} not found"
       )
   
-@router.delete('/tasks/{id}')
+@router.delete('/{id}', status_code=204)
 async def delete_task_by_id(id: int):
   try:
-    task = tm.remove_task(id)
-    return tm._task_to_dict(task)
+    tm.remove_task(id)
+    
   except KeyError:
     raise HTTPException(
       status_code = 404,
       detail = f"Task with id={id} not found"
       )
 
-@router.patch('/tasks/{id}')
+@router.patch('/{id}', response_model=TaskRead)
 async def complete_task_by_id(id: int):
   try:
     tm.complete_task(id)
+    task = tm.get_task(id)
+    return tm._task_to_dict(task)
   except KeyError:
     raise HTTPException(
       status_code = 404,
--- app/schemas/task.py
+++ app/schemas/task.py
@@ -2,3 +2,8 @@ from pydantic import BaseModel
 
 class TaskModel(BaseModel):
   title: str
+
+class TaskRead(BaseModel):
+  id: int
+  title: str
+  status: str
