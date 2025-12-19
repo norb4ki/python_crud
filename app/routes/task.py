d4a62909a391d23abdd850547a38d10a28349af7
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-12-19 09:03:12 +0200

    feat: add responses

--- app/routes/task.py
+++ app/routes/task.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from app.services.task_manager import TaskManager
-from app.schemas.task import TaskModel, TaskRead
+from app.schemas.task import *
 
 router = APIRouter(
   prefix='/tasks',
@@ -17,7 +18,13 @@ async def post_task(task: TaskModel):
   added_task = tm.add_task(task.title)
   return tm._task_to_dict(added_task)
 
-@router.get('/{id}', response_model=TaskRead)
+@router.get(
+    '/{id}', 
+    response_model=TaskRead,
+    responses={
+      404: {"model": NotFoundResponse, "description":f"Not Found"}
+    }   
+    )
 async def get_task_by_id(id: int):
   try:
     task = tm.get_task(id)
@@ -28,7 +35,13 @@ async def get_task_by_id(id: int):
       detail = f"Task with id={id} not found"
       )
   
-@router.delete('/{id}', status_code=204)
+@router.delete(
+    '/{id}', 
+    status_code=204,
+    responses={
+      404: {"model": NotFoundResponse, "description":f"Not Found"}
+    }  
+    )
 async def delete_task_by_id(id: int):
   try:
     tm.remove_task(id)
@@ -39,7 +52,13 @@ async def delete_task_by_id(id: int):
       detail = f"Task with id={id} not found"
       )
 
-@router.patch('/{id}', response_model=TaskRead)
+@router.patch(
+    '/{id}', 
+    response_model=TaskRead,
+    responses={
+      404: {"model": NotFoundResponse, "description":f"Not Found"}
+    }   
+    )
 async def complete_task_by_id(id: int):
   try:
     tm.complete_task(id)
--- app/schemas/task.py
+++ app/schemas/task.py
@@ -7,3 +7,9 @@ class TaskRead(BaseModel):
   id: int
   title: str
   status: str
+
+class BaseErrorResponse(BaseModel):
+  err: str
+
+class NotFoundResponse(BaseErrorResponse):
+  err:str = "Resource not found."
