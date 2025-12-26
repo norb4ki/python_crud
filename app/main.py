58865d25d6e02af3ed891791c33387383c49d6c6
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-12-26 10:19:22 +0200

    feat: add task service to the app state

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routes.task import router as task_router
 from dotenv import load_dotenv
+from app.repositories.task_repository import TaskRepository
+from app.services.task_manager import TaskService
 import asyncpg
 import os
 
@@ -10,15 +12,16 @@ load_dotenv()
 @asynccontextmanager
 async def lifespan(app: FastAPI):
   # Open the pool on startup
-  global db_pool
   try:
-    db_pool = await asyncpg.create_pool(
+    app.state.db_pool = await asyncpg.create_pool(
         user=os.getenv('DB_TASKS_USER'), 
         database=os.getenv('DB_TASKS_NAME'), 
         password=os.getenv('DB_TASKS_PASSWORD'),
         host='localhost',
         port=5432
         )
+    repo = TaskRepository(app.state.db_pool)
+    app.state.tm = TaskService(repo)
   except Exception as e:
     print("Connection failed:")
     print(e)
@@ -26,7 +29,7 @@ async def lifespan(app: FastAPI):
   yield
   # Close the pool after application is finished
   try:
-    await db_pool.close()
+    await app.state.db_pool.close()
     print('Connection is closed')
   except Exception as e:
     print("Exception raised during pool closing:")
