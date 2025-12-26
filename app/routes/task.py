cf77d9a6fb1eb5334d0298cc41889483fe6eb7f1
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-12-26 10:21:12 +0200

    feat: routes to use task manager  from the state

--- app/routes/task.py
+++ app/routes/task.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, HTTPException, Request
 from app.repositories.task_repository import TaskRepository
 from app.services.task_manager import TaskService
 from app.schemas.task import *
@@ -8,14 +7,19 @@ router = APIRouter(
   prefix='/tasks',
   tags=["tasks"]
   )
-tm = TaskService(repository=TaskRepository())
 
 @router.get('/', response_model=list[TaskRead])
-async def get_tasks():
+async def get_tasks(req: Request, ):
+  # Retrieving Task Manager from app state
+  tm = req.app.state.tm
+
   return tm.to_dict()
 
 @router.post('/', response_model=TaskRead)
-async def post_task(task: TaskModel):
+async def post_task(req: Request, task: TaskModel):
+    # Retrieving Task Manager from app state
+  tm = req.app.state.tm
+
   added_task = tm.add_task(task.title)
   return tm._task_to_dict(added_task)
 
@@ -26,7 +30,10 @@ async def post_task(task: TaskModel):
       404: {"model": NotFoundResponse, "description":f"Not Found"}
     }   
     )
-async def get_task_by_id(id: int):
+async def get_task_by_id(req: Request, id: int):
+  # Retrieving Task Manager from app state
+  tm = req.app.state.tm
+
   try:
     task = tm.get_task(id)
     return tm._task_to_dict(task)
@@ -43,7 +50,10 @@ async def get_task_by_id(id: int):
       404: {"model": NotFoundResponse, "description":f"Not Found"}
     }  
     )
-async def delete_task_by_id(id: int):
+async def delete_task_by_id(req: Request, id: int):
+  # Retrieving Task Manager from app state
+  tm = req.app.state.tm
+
   try:
     tm.remove_task(id)
     
@@ -60,7 +70,10 @@ async def delete_task_by_id(id: int):
       404: {"model": NotFoundResponse, "description":f"Not Found"}
     }   
     )
-async def complete_task_by_id(id: int):
+async def complete_task_by_id(req: Request, id: int):
+  # Retrieving Task Manager from app state
+  tm = req.app.state.tm
+
   try:
     tm.complete_task(id)
     task = tm.get_task(id)
