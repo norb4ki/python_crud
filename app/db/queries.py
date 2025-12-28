03825dad80b081061c70d0bf0cbdf102453bf8aa
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-12-28 11:18:56 +0200

    fix: insert, delete and update queries to return data

--- app/db/queries.py
+++ app/db/queries.py
@@ -1,6 +1,15 @@
 GET_TASKS = "SELECT * FROM tasks;"
 GET_TASK_BY_ID = "SELECT * FROM tasks WHERE id = $1;"
-POST_TASK = "INSERT INTO tasks (title) VALUES ($1);"
-COMPLETE_TASK = "UPDATE tasks SET is_completed = true WHERE id = $1;"
-DELETE_TASK = "DELETE FROM tasks WHERE id = $1;"
+POST_TASK = """
+INSERT INTO tasks (title) 
+VALUES ($1)
+RETURNING id, title, completed;
+"""
+COMPLETE_TASK = """
+UPDATE tasks SET is_completed = true WHERE id = $1
+RETURNING id, title, completed;
+"""
+DELETE_TASK = """DELETE FROM tasks WHERE id = $1
+RETURNING id;
+"""
 VALIDATE_CONNECTION = "SELECT 1;"
