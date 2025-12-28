c30c4957af2e83295b75eb0a29c87edd34492f84
Vladimir Baraniuk <vladimirbaraniuk@gmail.com>
2025-12-28 09:17:01 +0200

    refactor: add completed prop as optional for Task

--- app/models/task.py
+++ app/models/task.py
@@ -1,8 +1,8 @@
 class Task:
-  def __init__(self, id: int, title:str):
+  def __init__(self, id: int, title:str, completed:bool = False):
     self.id = id
     self.title = title
-    self.completed: bool = False
+    self.completed: bool = completed
 
   @property
   def status(self):
